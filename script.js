9f62ecfa33cd802bcf3d82f7d0e8d393b7c1bc22
Felipe <felipe__andrew@hotmail.com>
2022-07-20 12:05:25 -0300

    requisitos faltantes

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const btn = document.getElementById('btn-login');
 const emailLogin = document.getElementById('email-login');
 const emailSenha = document.getElementById('senha-login');
+
+const submitBtn = document.getElementById('submit-btn');
+const checkBox = document.getElementById('agreement');
+
 btn.addEventListener('click', () => {
   if (emailLogin.value === 'tryber@teste.com' && emailSenha.value === '123456') {
     alert('Olá, Tryber!');
@@ -8,3 +12,8 @@ btn.addEventListener('click', () => {
     alert('Email ou senha inválidos.');
   }
 });
+
+
+checkBox.addEventListener('click', () => {
+  submitBtn.disabled = false
+})
